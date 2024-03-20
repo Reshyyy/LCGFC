550678cded56d5c6f625c337bcf39511f7c1cb46
Reshyyy <riyadhbrailleanthonyligan@gmail.com>
2024-03-20 08:21:27 +0800

    conditional rendering

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,6 +3,11 @@
 @section('title', $post['title'])
 
 @section('content')
+@if($post['is_new'])
+<div>A new blog post using if</div>
+@else(!$post['is_new'])
+<div>blog post is old using elseif/else</div>
+@endif
 <h1>{{ $post['title'] }}</h1>
 <p>{{ $post['content'] }}</p>
 @endsection
